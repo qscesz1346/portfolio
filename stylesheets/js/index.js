5734ca7e9198d6ba996c4eef2c0f07e942f297be
qscesz1346 <qscesz1346@naver.com>
2024-01-02 20:52:28 +0900

    2024.01.02

--- stylesheets/js/index.js
+++ stylesheets/js/index.js
@@ -33,7 +33,7 @@ const CIRCULATION_INTERVAL = 5000;
 
 const splashImages = window.document.body.querySelectorAll(':scope > div.home > section.splash > div');
 for (let i = 0; i < splashImages.length; i++) {
-    splashImages[i].style.backgroundImage = `url("../stylesheets/images/index-${i + 1}.jpg")`;
+    splashImages[i].style.backgroundImage = `url("./stylesheets/images/index-${i + 1}.jpg")`;
     splashImages[i].style.objectFit = 'cover';
     splashImages[i].style.width = '100%';
     splashImages[i].style.height = '100%';
@@ -56,6 +56,12 @@ const hamNav = document.getElementById('hamNav');
 const aTags = hamNav.getElementsByTagName('a');
 const liTags = hamNav.getElementsByTagName('li');
 
+const isMobile = window.matchMedia("(max-width: 767px)").matches;
+const isTablet = window.matchMedia("(max-width: 1023px)").matches;
+
+// 모바일 체크
+// function Mobile() {return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
+
 $(document).ready(function() {
     
     for (let i = 0; i < sideNav.length; i++) {
@@ -64,9 +70,6 @@ $(document).ready(function() {
                 el.classList.remove("selected");
             }
             sideNav[i].classList.add("selected");
-
-            const isMobile = window.matchMedia("(max-width: 767px)").matches;
-            const isTablet = window.matchMedia("(max-width: 1023px)").matches;
             
             const location = sideNav[i].getElementById;
             if(isMobile) {
@@ -88,11 +91,11 @@ $(document).ready(function() {
 
             if(scrollLocation>=0 && scrollLocation <= 850) {
                 sideNav[0].classList.add('selected');
-            } else if(scrollLocation >= 850 && scrollLocation <= 1330) {
+            } else if(scrollLocation >= 850 && scrollLocation <= 2235) {
                 sideNav[1].classList.add('selected');
-            } else if(scrollLocation >= 1330 && scrollLocation <= 2250) {
+            } else if(scrollLocation >= 2235 && scrollLocation <= 3210) {
                 sideNav[2].classList.add('selected');
-            } else if(scrollLocation >= 2250 && scrollLocation <= 3800) {
+            } else if(scrollLocation >= 3210 && scrollLocation <= 5220) {
                 sideNav[3].classList.add('selected');
             } else {
                 sideNav[4].classList.add('selected');
@@ -101,92 +104,34 @@ $(document).ready(function() {
         });
         
     };
-
-    for (let i = 0; i < aTags.length; i++) {
-        aTags[i].addEventListener("click", function(event) {
-            event.preventDefault(); // 링크 클릭 시 페이지 이동 방지
-    
-            for (const el of aTags) {
-                el.classList.remove("selected");
-            }
-            aTags[i].classList.add("selected");
-    
-            const isMobile = window.matchMedia("(max-width: 767px)").matches;
-            const isTablet = window.matchMedia("(max-width: 1023px)").matches;
-            
-            const href = aTags[i].getAttribute('href');
-            const targetId = href.substring(1); // '#' 제거
-            const targetElement = document.getElementById(targetId);
-            
-            if (targetElement) {
-                const location = targetElement.offsetTop;
-                console.log(location);
-                
-                if (isMobile) {
-                    if(targetId != "top") {
-                        window.scrollTo({
-                            top: location + 360,
-                            behavior: "smooth"
-                        }); 
-                    }
-                }
-            };
-            if(targetId == "top") {
-                window.scrollTo(0,0);
-            };
-
-        });
-
-        window.addEventListener("scroll", () => {
-            const scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
-
-            console.log(scrollLocation);
-
-            for (const el of liTags) {
-                el.classList.remove("selected");
-            }
-
-            if(scrollLocation>=0 && scrollLocation <= 380) {
-                liTags[0].classList.add('selected');
-            } else if(scrollLocation >= 381 && scrollLocation <= 549) {
-                liTags[1].classList.add('selected');
-            } else if(scrollLocation >= 550 && scrollLocation <= 1446) {
-                liTags[2].classList.add('selected');
-            } else if(scrollLocation >= 1447 && scrollLocation <= 3049) {
-                liTags[3].classList.add('selected');
-            } else {
-                liTags[4].classList.add('selected');
-            }
-
-        });
-    }
 });
 
-
-$(function(){
-    // 	이미지 클릭시 해당 이미지 모달
-    $(".swiper-slide").click(function(){
-        $(".modal").show();
-        // 해당 이미지 가져오기
-        var imgSrc = $(this).children("img").attr("src");
-        var imgAlt = $(this).children("img").attr("alt");
-        $(".modal-box img").attr("src", imgSrc);
-        $(".modal-box img").attr("alt", imgAlt);
-    });
+$(function () {
+    if(!isMobile) {
+        // 	이미지 클릭시 해당 이미지 모달
+        $(".swiper-slide").click(function(){
+            $(".modal").show();
+            // 해당 이미지 가져오기
+            var imgSrc = $(this).children("img").attr("src");
+            var imgAlt = $(this).children("img").attr("alt");
+            $(".modal-box img").attr("src", imgSrc);
+            $(".modal-box img").attr("alt", imgAlt);
+        });
         
-    //.modal안에 button을 클릭하면 .modal닫기
-    $(".modal button").click(function(){
-        $(".modal").hide();
-    });
+        //.modal안에 button을 클릭하면 .modal닫기
+        $(".modal button").click(function(){
+            $(".modal").hide();
+        });
         
-    //.modal밖에 클릭시 닫힘
-    $(".modal").click(function (e) {
-        if (e.target.className != "modal") {
-            return false;
-        } else {
-          $(".modal").hide();
-        }
-    });
+        //.modal밖에 클릭시 닫힘
+        $(".modal").click(function (e) {
+            if (e.target.className != "modal") {
+                return false;
+            } else {
+                $(".modal").hide();
+            }
+        });
+    }
 });
 
 //미디어쿼리 햄버거드롭다운
@@ -230,4 +175,84 @@ $(burger).on("click", function () {
             h=0;
         }
     });
+});
+
+$('.ham-nav li a').on("click", function () {
+    $('.ham-con').animate({
+        right: '-100%',
+        opacity: 0
+    }, 1000);
+    $(burger).removeClass('active-1');
+    h = 0;
+});
+
+// 메인화면 타이핑효과
+const target1 = document.querySelector("#typingCursor1");
+const target2 = document.querySelector("#typingCursor2");
+
+function blink() {  // 커서 깜빡이는 효과
+    target2.classList.toggle("active");
+}
+
+const string1 = "Welcome, LDG's Portfolio";
+const string2 = "WEB DEVELOPER";
+const split1 = string1.split("");
+const split2 = string2.split("");
+
+// 첫줄 타이핑효과
+function typing1(arr) {
+    if (arr.length > 0) {
+        target1.textContent += arr.shift();
+        setTimeout(function() {
+            typing1(arr)}, 80)
+    }
+}
+// 두번째 줄 타이핑효과
+function typing2(arr) {
+    if (arr.length > 0) {
+        target2.textContent += arr.shift();
+        setTimeout(function() {
+            typing2(arr);
+        }, 80)
+    } else {
+        target2.classList.add('cursorVisible');
+        setTimeout(function() {setInterval(blink, 500)}, 500);
+    }
+}
+typing1(split1);
+setTimeout(function() {typing2(split2)}, 2000);
+
+
+// 미디어 쿼리 햄버거메뉴 클릭시 작동
+
+const mainDiv = document.querySelectorAll("#main > div:not(.back-to-top)");
+
+$(document).ready(function() {
+
+    for (let i = 0; i < aTags.length; i++) {
+        aTags[i].addEventListener("click", function(event) {
+            event.preventDefault(); // 링크 클릭 시 페이지 이동 방지
+    
+            for (const lt of liTags) {
+                lt.classList.remove("selected");
+            }
+            liTags[i].classList.add("selected");
+            const aTagsHref = this.getAttribute('href');
+            const aTagsId = aTagsHref.substring(1);
+            const aTagsElement = document.getElementById(aTagsId);
+            const mainBG = document.getElementById("main");
+            console.log(aTagsId);
+            for (const md of mainDiv) {
+                md.style.display = 'none';
+            }
+            if (aTagsId == "top") {
+                mainBG.style.display = 'none';
+            } else {
+                mainBG.style.display = 'block';
+                aTagsElement.style.display = 'block';
+            }
+
+            
+        });
+    }
 });
